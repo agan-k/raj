3d7ce104d55066dbfb55adb4b69a5d8609f3cd2d
Koran Agan <koranagan@gmail.com>
2020-06-19 20:37:40 -0400

    Router and components boiler plate add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom"
+import Landing from './components/Landing'
+import Home from './components/Home'
+import About from './components/About'
+import Discography from './components/Discography'
+import Videos from './components/Videos'
+import Photos from './components/Photos'
+import Calendar from './components/Calendar'
+import Store from './components/Store'
+import Header from './Header'
+import Footer from './Footer'
+
+import './SharedStyle.css'
+
 
 function App() {
   return (
     <div className="App">
-        <h1>hello
-            
-     </h1>
+        <Header />
+
+        <Switch>
+           <Route exact path='/'>
+              <Landing />
+           </Route>
+           <Route path='/home'>
+              <Home />
+           </Route>
+           <Route path='/about'>
+              <About />
+           </Route>
+           <Route path='/discography'>
+              <Discography />
+           </Route>
+           <Route path='/videos'>
+              <Videos />
+           </Route>
+           <Route path='/photos'>
+              <Photos />
+           </Route>
+           <Route path='/calendar'>
+              <Calendar />
+           </Route>
+           <Route path='/store'>
+              <Store />
+           </Route>
+           
+        </Switch>
+        <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
