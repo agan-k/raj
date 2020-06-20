7c5e2c470933d045a7df0b9850f988508db322e0
Koran Agan <koranagan@gmail.com>
2020-06-20 08:26:50 -0400

    landing page w/CTA

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import pistils from '../assets/albums/pistils.jpg'
 
 export default function Landing() {
    return (
-      <div>
-         <h1>Landing</h1>
+      <div className="landing">
+         <div className="album-container">
+            <img src={pistils} />
+            <div className="out-now">
+               <p>PISTILS</p>
+               <br></br>
+            <p>
+               featuring <br></br>
+               Chris Cheek,<br></br>
+               Aaron Goldberg,<br></br> 
+               Lara Bello and<br></br>
+               Sam Aning
+            </p>
+               <button>listen/buy</button><br></br>
+               <Link id="enter" to="/home">enter site >></Link>
+            </div>
+         </div>
       </div>
    )
 }
