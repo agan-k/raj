529a905db4448c1ae79449fc0420603274a316bf
Koran Agan <koranagan@gmail.com>
2020-07-01 08:51:26 -0400

    style HOME masonry grid flexbox,

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,17 @@ export default function Home() {
    return (
       <div class="container">
          <div class="item">
-         {/* <img src={pistils} style={{ }} /> */}
+            <div
+               className="mask"
+               style={{
+                  height: '9em',
+                  background: 'url(./assets/backgrounds/1a.jpg)',
+                  overflow: 'hidden'
+               }}
+            >
+            <img src={pistils} />
+            </div>
+            
              <h2>1 New album "Pistils" out now!</h2>
              <p>Rajiv Jayaweera will release his debut album, Pistils on June 5, 2020. Featuring Chris Cheek, 
              Aaron Parks, Lara Bello, Hugh Stuckey and Sam Anning, it will come out on Outside in Music (USA) 
@@ -25,20 +35,27 @@ export default function Home() {
             </p>
          </div>
          <div class="item">
+            <div className="video">
                 <ReactPlayer url='https://youtu.be/n1lvfw0MWMo' width="100%" height="15em"/>
+            </div>
             <h2>2 Ellstandissa single released!!</h2>
 
             <p>Description of the video here.Description of the video here.Description of the video here.
             Description of the video here.Description of the video here.Description of the video here.
             Description of the video here.Description of the video here.Description of the video here.
-            Description of the video here.Description of the video here.Description of the video here.
-            Description of the video here.Description of the video here.Description of the video here.
             </p>
-            
           </div>
-         
          <div class="item">
-         <img src={laniakea} style={{  }} />
+         <div
+               className="mask"
+               style={{
+                  height: '9em',
+                  background: 'url(./assets/backgrounds/1a.jpg)',
+                  overflow: 'hidden'
+               }}
+            >
+            <img src={laniakea} />
+            </div>
             <h2>3 New Trio Kleine Ahnung album "Laniakea" out now!</h2>
             <p>Laniakea is the new album from Trio Kleine Ahnung, a collaboration between guitarist Carl Morgan, 
             bassist Sam Anning and drummer Raj Jayaweera. Spawned from a week off from a busy European tour 
@@ -47,33 +64,27 @@ export default function Home() {
             </p>
          </div>
          <div class="item">
-         <img src={pistils} style={{ }} />
-             <h2>4 New album "Pistils" out now!</h2>
-             <p>Rajiv Jayaweera will release his debut album, Pistils on June 5, 2020. Featuring Chris Cheek, 
-             Aaron Parks, Lara Bello, Hugh Stuckey and Sam Anning, it will come out on Outside in Music (USA) 
-             and Earshift Music (Australia).
-             <br />
-             <br />
-             To pre-order a CD, vinyl LP or digital copy, you can visit pistils.bandcamp.com
-             <br />  
-             <br />  
-             Unfortunately, Rajiv’s scheduled New York album release show at Birdland Jazz Club on May 3rd
-             was cancelled due to the current situation.
-            </p>
-         </div>
-         <div class="item">
+         <div className="video">
                 <ReactPlayer url='https://youtu.be/n1lvfw0MWMo' width="100%" height="15em"/>
-            <h2>5 Ellstandissa single released!!</h2>
+            </div>
+            <h2>4 Ellstandissa single released!!</h2>
             <p>Description of the video here.Description of the video here.Description of the video here.
-            Description of the video here.Description of the video here.Description of the video here.
-            Description of the video here.Description of the video here.Description of the video here.
             Description of the video here.Description of the video here.Description of the video here.
             Description of the video here.Description of the video here.Description of the video here.
             </p>
           </div>
          <div class="item">
-         <img src={pistils} style={{ }} />
-             <h2>6 New album "Pistils" out now!</h2>
+         <div
+               className="mask"
+               style={{
+                  height: '9em',
+                  background: 'url(./assets/backgrounds/1a.jpg)',
+                  overflow: 'hidden'
+               }}
+            >
+            <img src={pistils} />
+            </div>
+             <h2>5 New album "Pistils" out now!</h2>
              <p>Rajiv Jayaweera will release his debut album, Pistils on June 5, 2020. Featuring Chris Cheek, 
              Aaron Parks, Lara Bello, Hugh Stuckey and Sam Anning, it will come out on Outside in Music (USA) 
              and Earshift Music (Australia).
@@ -87,18 +98,27 @@ export default function Home() {
             </p>
          </div>
          <div class="item">
+         <div className="video">
                 <ReactPlayer url='https://youtu.be/n1lvfw0MWMo' width="100%" height="15em"/>
-            <h2>7 Ellstandissa single released!!</h2>
+            </div>
+            <h2>6 Ellstandissa single released!!</h2>
             <p>Description of the video here.Description of the video here.Description of the video here.
-            Description of the video here.Description of the video here.Description of the video here.
-            Description of the video here.Description of the video here.Description of the video here.
             Description of the video here.Description of the video here.Description of the video here.
             Description of the video here.Description of the video here.Description of the video here.
             </p>
           </div>
          <div class="item">
-         <img src={pistils} style={{ }} />
-             <h2>8 New album "Pistils" out now!</h2>
+         <div
+               className="mask"
+               style={{
+                  height: '9em',
+                  background: 'url(./assets/backgrounds/1a.jpg)',
+                  overflow: 'hidden'
+               }}
+            >
+            <img src={laniakea} />
+            </div>
+             <h2>7 New album "Pistils" out now!</h2>
              <p>Rajiv Jayaweera will release his debut album, Pistils on June 5, 2020. Featuring Chris Cheek, 
              Aaron Parks, Lara Bello, Hugh Stuckey and Sam Anning, it will come out on Outside in Music (USA) 
              and Earshift Music (Australia).
@@ -112,8 +132,51 @@ export default function Home() {
             </p>
          </div>
          <div class="item">
-         <img src={laniakea} style={{  }} />
-            <h2>9 New Trio Kleine Ahnung album "Laniakea" out now!</h2>
+         <div className="video">
+                <ReactPlayer url='https://youtu.be/n1lvfw0MWMo' width="100%" height="15em"/>
+            </div>
+            <h2>8 Ellstandissa single released!!</h2>
+            <p>Description of the video here.Description of the video here.Description of the video here.
+            Description of the video here.Description of the video here.Description of the video here.
+            Description of the video here.Description of the video here.Description of the video here.
+            </p>
+          </div>
+         <div class="item">
+         <div
+               className="mask"
+               style={{
+                  height: '9em',
+                  background: 'url(./assets/backgrounds/1a.jpg)',
+                  overflow: 'hidden'
+               }}
+            >
+            <img src={pistils} />
+            </div>
+             <h2>9 New album "Pistils" out now!</h2>
+             <p>Rajiv Jayaweera will release his debut album, Pistils on June 5, 2020. Featuring Chris Cheek, 
+             Aaron Parks, Lara Bello, Hugh Stuckey and Sam Anning, it will come out on Outside in Music (USA) 
+             and Earshift Music (Australia).
+             <br />
+             <br />
+             To pre-order a CD, vinyl LP or digital copy, you can visit pistils.bandcamp.com
+             <br />  
+             <br />  
+             Unfortunately, Rajiv’s scheduled New York album release show at Birdland Jazz Club on May 3rd
+             was cancelled due to the current situation.
+            </p>
+         </div>
+         <div class="item">
+         <div
+               className="mask"
+               style={{
+                  height: '9em',
+                  background: 'url(./assets/backgrounds/1a.jpg)',
+                  overflow: 'hidden'
+               }}
+            >
+            <img src={laniakea} />
+            </div>
+            <h2>10 New Trio Kleine Ahnung album "Laniakea" out now!</h2>
             <p>Laniakea is the new album from Trio Kleine Ahnung, a collaboration between guitarist Carl Morgan, 
             bassist Sam Anning and drummer Raj Jayaweera. Spawned from a week off from a busy European tour 
             schedule, the members contribute stand-out compositions and performances that draw on inspiration 
